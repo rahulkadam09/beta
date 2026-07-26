fa84fe1ff035eee0a83cc11687b96f25edc7e8c5
rahulkadam09 <rahulkadam2210@gmail.com>
2026-07-26 12:06:14 +0530

    index.html updated for color

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/api')
+def api():
+    return {'message': 'Hello, World!'}
+
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', port=9000)
